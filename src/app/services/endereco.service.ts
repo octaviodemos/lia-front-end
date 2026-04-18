3f63f416ba1c951ed15aa4d912549a6d29dfb557
Octávio Demos <octaviodemos@gmail.com>
2026-04-18 19:05:52 -0300

    fix: route endereço

--- src/app/services/endereco.service.ts
+++ src/app/services/endereco.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,21 @@ export class EnderecoService {
 
   private apiUrl = 'http://localhost:3333/api/addresses';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return token ? new HttpHeaders({ Authorization: `Bearer ${token}` }) : new HttpHeaders();
+  }
 
   getEnderecos(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return this.http.get(this.apiUrl, { headers: this.getAuthHeaders() });
   }
 
   addEndereco(endereco: any): Observable<any> {
-    return this.http.post(this.apiUrl, endereco);
+    return this.http.post(this.apiUrl, endereco, { headers: this.getAuthHeaders() });
   }
 }
